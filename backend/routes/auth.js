def72b449c734248f6b777f96978fac294726480
umarshakoor <umarshakoor70@outlook.com>
2024-06-07 10:04:45 +0500

    xnotebook

---
+++ backend/routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User')
+
+router.post('/', (req, res) => {
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
+})
+module.exports = router
